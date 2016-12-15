abb61598bb6cae29d705b653a9dcdca4b1abc363
AukeSchaap <A.C.Schaap@student.tudelft.nl>
2016-12-15 20:55:21 +0100

    addTodo() finished, it now adds a button that removes the corresponding todo item

--- js/list.js
+++ js/list.js
@@ -19,25 +19,19 @@ function addTodo() {
     var priority = document.getElementById("priority").value;
     var node = document.createElement("li");
 
-    node.innerHTML = priority + ": " + name + " ";
+    node.innerHTML = priority + ": " + name + " " + document.getElementById("myList").childElementCount;
 
     // Adds a remove button to the list
     var node2 = document.createElement("input");
     node2.type = "button";
     node2.onclick = function () {
-        removeTodo();
+        document.getElementById("myList").removeChild(node);
     };
     node2.value = "Remove this element";
     node.appendChild(node2);
     document.getElementById("myList").appendChild(node);
 }
 
-/**
- * Removes the last 2 items from the list which are the to do item and the button.
- */
-function removeTodo() {
-    ul.removeChild(ul.lastElementChild);
-}
 
 function changeList() {
     window.location.href = '../html/Home.html';
