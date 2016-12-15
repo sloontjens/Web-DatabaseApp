69fbcb0f7266fbe0216287514baa24c15b307c69
AukeSchaap <A.C.Schaap@student.tudelft.nl>
2016-12-15 20:33:32 +0100

    addTodo() now adds a todo item with given priority, name and a button to remove it

--- js/list.js
+++ js/list.js
@@ -10,21 +10,33 @@ function searchLists() {
 }
 
 /**
- * Adds an textual input item to the list
+ * Adds an textual input item to the list and a button to remove it.
  */
-function addList() {
-    var List_input = document.getElementById("L_inp").value;
-    var text_input = document.createTextNode(List_input);
-    var node = document.createElement("LI");
-    node.appendChild(text_input);
+function addTodo() {
+
+    // Adds a new to-do item to the list
+    var name = document.getElementById("name").value;
+    var priority = document.getElementById("priority").value;
+    var node = document.createElement("li");
+
+    node.innerHTML = priority + ": " + name + " ";
+
+    // Adds a remove button to the list
+    var node2 = document.createElement("input");
+    node2.type = "button";
+    node2.onclick = function () {
+        removeTodo();
+    };
+    node2.value = "Remove this element";
+    node.appendChild(node2);
     document.getElementById("myList").appendChild(node);
 }
 
 /**
- * Removes the last item from the list
+ * Removes the last 2 items from the list which are the to do item and the button.
  */
-function removeListLast() {
-    document.getElementById("myList").removeChild(document.getElementById("myList").lastElementChild);
+function removeTodo() {
+    ul.removeChild(ul.lastElementChild);
 }
 
 function changeList() {
